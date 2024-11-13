8f5e4c342aa8481a43db9ca0cd73f9f4a61b96be
baldraven <elyaselaziz@gmail.com>
2024-11-13 17:19:42 +0100

    bench: first benches using criterion. gpu is not working properly yet

---
+++ benches/jfa.rs
@@ -0,0 +1,16 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use blue_noise::jfa_cpu::jfa;
+use blue_noise::jfa_wgpu::run;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let points = vec![(1.0, 1.0), (2.0, 2.0), (3.0, 3.0), (4.0, 4.0), (5.0, 5.0)];
+    
+    let mut group = c.benchmark_group("jfa");
+    group.sample_size(10);
+    //group.bench_function("jfa_cpu", |b| b.iter(|| jfa(black_box(&points), black_box((10.,10.)))));
+    group.bench_function("jfa_gpu", |b| b.iter(|| run(black_box(&points), black_box((10.,10.)))));
+    group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
